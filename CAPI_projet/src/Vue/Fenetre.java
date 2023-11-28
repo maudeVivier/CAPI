9e538bf9a4a0ca1b8133590019a158e55eb54f84
Fafishyle <finaritralft@gmail.com>
2023-11-28 12:03:32 +0100

    update

--- CAPI_projet/src/Vue/Fenetre.java
+++ CAPI_projet/src/Vue/Fenetre.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Graphics;
 
+
 public class Fenetre extends JFrame{
     private JPanel Panel1;
     public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -18,14 +19,14 @@ public class Fenetre extends JFrame{
         setSize(screenWidth, screenHeight);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
+/*
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         setBackground(new Color(0,0,50));
          setLayout(null);
          Affichage.PageAccueil(g);
     }
-
+*/
     public static void main(String[] args) {
         Fenetre t = new Fenetre();
         t.setVisible(true);
