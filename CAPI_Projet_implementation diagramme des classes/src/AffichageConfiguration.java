bd07e1ebcd1b8df80c4df790d2c4091bf7b27084
Maude <chrismaude@hotmail.fr>
2023-12-03 18:51:02 +0100

    modification de l'affichage
    affichage en cours d'execution mais recuperation des pseudos de tous les joueurs et vérification lors de la validation des pseudos.
    Affichage va changer plus tard

--- CAPI_Projet_implementation diagramme des classes/src/AffichageConfiguration.java
+++ CAPI_Projet_implementation diagramme des classes/src/AffichageConfiguration.java
@@ -4,19 +4,73 @@ import java.awt.event.*;
 import java.util.*;
 
 public class AffichageConfiguration extends JPanel {
+    private JPanel accueilPanel = new JPanel();
+    private JPanel nbJoueurPanel = new JPanel();
+    private JPanel pseudoPanel = new JPanel();
 
-    /*Fonction ou on va mettre des actions avec des touches claviers,
-    toucher un bouton, ect
-    */
     public AffichageConfiguration(){
-        this.setLayout(null);
+        accueilPanel.setLayout(new BoxLayout(accueilPanel, BoxLayout.Y_AXIS));
+
+        // Titre
+        AffichageInfo.labelTitreAccueil.setFont(new Font("Arial", Font.PLAIN, AffichageInfo.sizeTitre));
+        AffichageInfo.labelTitreAccueil.setForeground(Color.WHITE);
+        AffichageInfo.labelTitreAccueil.setAlignmentX(Component.CENTER_ALIGNMENT);
+        accueilPanel.add(AffichageInfo.labelTitreAccueil);
+
+        // Ajouter un espace vertical
+        accueilPanel.add(Box.createVerticalStrut(AffichageInfo.screenHeight / 5));
+
+        // Bouton
+        AffichageInfo.boutonNouvellePartie.setFont(new Font("Calibri", Font.BOLD, AffichageInfo.sizeTexte));
+        AffichageInfo.boutonNouvellePartie.setBackground(Color.darkGray);
+        AffichageInfo.boutonNouvellePartie.setForeground(Color.WHITE);
+        AffichageInfo.boutonNouvellePartie.setAlignmentX(Component.CENTER_ALIGNMENT);
+        accueilPanel.add(AffichageInfo.boutonNouvellePartie);
+
+        accueilPanel.setBackground(new Color(0, 0, 50));
+        accueilPanel.setPreferredSize(new Dimension(AffichageInfo.screenWidth, AffichageInfo.screenHeight));
+        add(accueilPanel);
 
         /* -------------------Bouton pour parametrer le nombre de joueur ainsi que les pseudos---------------------- */
         AffichageInfo.boutonNouvellePartie.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                AffichageInfo.menu = 1;
-                repaint();
+
+                nbJoueurPanel.setLayout(new BoxLayout(nbJoueurPanel, BoxLayout.Y_AXIS));
+                // Menu pour choisir le nombre de joueurs
+                AffichageInfo.labelTitreNbJoueur.setFont(new Font("Arial", Font.PLAIN, AffichageInfo.sizeTitre));
+                AffichageInfo.labelTitreNbJoueur.setForeground(Color.white);
+                AffichageInfo.labelTitreNbJoueur.setAlignmentX(Component.CENTER_ALIGNMENT);
+                nbJoueurPanel.add(AffichageInfo.labelTitreNbJoueur);
+
+                // Ajouter un espace vertical
+                nbJoueurPanel.add(Box.createVerticalStrut(AffichageInfo.screenHeight / 5));
+
+                AffichageInfo.labelNbJoueur.setFont(new Font("Arial", Font.PLAIN, AffichageInfo.sizeTexte));
+                AffichageInfo.labelNbJoueur.setForeground(Color.white);
+                AffichageInfo.labelNbJoueur.setAlignmentX(Component.CENTER_ALIGNMENT);
+                nbJoueurPanel.add(AffichageInfo.labelNbJoueur);
+
+                AffichageInfo.boutonValiderNbJoueur.setFont(new Font("Calibri", Font.BOLD, AffichageInfo.sizeTexte));
+                AffichageInfo.boutonValiderNbJoueur.setBackground(Color.darkGray);
+                AffichageInfo.boutonValiderNbJoueur.setForeground(Color.WHITE);
+                AffichageInfo.boutonValiderNbJoueur.setAlignmentX(Component.RIGHT_ALIGNMENT);
+                nbJoueurPanel.add(AffichageInfo.boutonValiderNbJoueur);
+
+                //nbJoueurPanel.add(Box.createVerticalStrut(AffichageInfo.screenHeight / 10));
+
+                JSpinner.DefaultEditor editor = (JSpinner.DefaultEditor) AffichageInfo.nb_joueur_Spinner.getEditor();
+                editor.getTextField().setFont(new Font("Arial", Font.PLAIN, AffichageInfo.sizeTexte));
+                AffichageInfo.nb_joueur_Spinner.setEnabled(true);
+                nbJoueurPanel.add(AffichageInfo.nb_joueur_Spinner);
+
+                //nbJoueurPanel.add(Box.createVerticalStrut(AffichageInfo.screenHeight / 5));
+
+                nbJoueurPanel.setBackground(new Color(0, 0, 50));
+                nbJoueurPanel.setPreferredSize(new Dimension(AffichageInfo.screenWidth, AffichageInfo.screenHeight));
+                add(nbJoueurPanel);
+
+                setMenu(1);
             }
         });
 
@@ -25,93 +79,125 @@ public class AffichageConfiguration extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AffichageInfo.nbJoueur = (int) AffichageInfo.nb_joueur_Spinner.getValue();
-                AffichageInfo.menu = 2;
-                System.out.print("Nombre de joueur : " + AffichageInfo.nbJoueur);
-                repaint();
+                pseudoPanel.setLayout(new BorderLayout());
+
+                AffichageInfo.labelTitrePseudoJoueur.setFont(new Font("Arial", Font.PLAIN, AffichageInfo.sizeTitre));
+                AffichageInfo.labelTitrePseudoJoueur.setForeground(Color.WHITE);
+                AffichageInfo.labelTitrePseudoJoueur.setHorizontalAlignment(JLabel.CENTER);
+                pseudoPanel.add(AffichageInfo.labelTitrePseudoJoueur, BorderLayout.NORTH);
+
+                JPanel textAreaPanel = new JPanel();
+                initialiserComposants(textAreaPanel);
+
+                textAreaPanel.setBackground(new Color(0, 0, 50));
+                // Ajouter le panneau de JTextArea au centre du BorderLayout
+                pseudoPanel.add(textAreaPanel, BorderLayout.CENTER);
+
+                AffichageInfo.boutonValiderPseudo.setFont(new Font("Calibri", Font.BOLD, AffichageInfo.sizeTexte));
+                AffichageInfo.boutonValiderPseudo.setBackground(Color.darkGray);
+                AffichageInfo.boutonValiderPseudo.setForeground(Color.WHITE);
+
+                pseudoPanel.add(AffichageInfo.boutonValiderPseudo,BorderLayout.WEST);
+
+                pseudoPanel.setBackground(new Color(0, 0, 50));
+                pseudoPanel.setPreferredSize(new Dimension(AffichageInfo.screenWidth, AffichageInfo.screenHeight));
+                add(pseudoPanel);
+                setMenu(2);
             }
         });
-
 
         /* -------------------Bouton pour valider les pseudos---------------------- */
         AffichageInfo.boutonValiderPseudo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                AffichageInfo.menu = 3;
-                System.out.println("Nombre de joueur : " + AffichageInfo.nbJoueur);
                 afficherContenuPseudos();
-
-                /*
-                if (verifierPseudosNonNuls()) {
+                if (verifierPseudosNonNuls() && verifierPseudosUniques()) {
                     System.out.println("Tous les pseudos sont valides.");
+                    setMenu(3);
                 } else {
                     System.out.println("Certains pseudos sont manquants ou identiques.");
-                }*/
-                repaint();
+                }
             }
         });
-
     }
 
+    public void setMenu(int menu) {
+        switch (menu) {
+            case 0:
+                // Affichage de l'accueil
+                accueilPanel.setVisible(true);
+                nbJoueurPanel.setVisible(false);
+                pseudoPanel.setVisible(false);
+                break;
+            case 1:
+                // Affichage du menu pour choisir le nombre de joueurs
+                accueilPanel.setVisible(false);
+                nbJoueurPanel.setVisible(true);
+                pseudoPanel.setVisible(false);
+                break;
+            case 2:
+                // Affichage du menu pour choisir les pseudos
+                accueilPanel.setVisible(false);
+                nbJoueurPanel.setVisible(false);
+                pseudoPanel.setVisible(true);
+                break;
+        }
+    }
+
+    private void initialiserComposants(JPanel panel) {
+        panel.setLayout(new GridLayout(0, 2, 20, 20)); // 2 colonnes, espacement de 20 pixels
+
+        AffichageInfo.areaTabPseudo = new JTextArea[AffichageInfo.nbJoueur];
+
+        for (int i = 0; i < AffichageInfo.nbJoueur; i++) {
+            JTextArea areaPseudo = new JTextArea(1, 1); // 1 ligne, 20 colonnes
+            areaPseudo.setFont(new Font("Arial", Font.BOLD, AffichageInfo.sizeTexte));
+
+            // Ajoutez la JTextArea au GridLayout
+            panel.add(areaPseudo);
+
+            AffichageInfo.areaTabPseudo[i] = areaPseudo;
+        }
+    }
     /*
     Fonction pour modifier l'apparence de la fenetre
     enlever bouton, rajouter bouton, ect
-     */
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         setBackground(new Color(0, 0, 50));
 
         if (AffichageInfo.menu == 0) { //accueil
-            add(AffichageInfo.boutonNouvellePartie);
+            //add(AffichageInfo.boutonNouvellePartie);
             Affichage.PageAccueil(g);
-        } else if(AffichageInfo.menu == 1){ //menu pour choisir nombre de joueurs
-            remove(AffichageInfo.boutonNouvellePartie);
+        } else if (AffichageInfo.menu == 1) { //menu pour choisir nombre de joueurs
+            //remove(AffichageInfo.boutonNouvellePartie);
 
-            add(AffichageInfo.boutonValiderNbJoueur);
+            //add(AffichageInfo.boutonValiderNbJoueur);
 
-            setLayout(new FlowLayout());
-            add(AffichageInfo.nb_joueur_Spinner);
+            //setLayout(new FlowLayout());
+            //add(AffichageInfo.nb_joueur_Spinner);
 
             Affichage.MenuConfigPartie(g);
-            setFocusable(true);
-        } else if(AffichageInfo.menu == 2){ //menu pour choisir les pseudos
+            //setFocusable(true);
+        } else if (AffichageInfo.menu == 2) { //menu pour choisir les pseudos
             remove(AffichageInfo.boutonValiderNbJoueur);
             remove(AffichageInfo.nb_joueur_Spinner);
 
-            int x0 = (AffichageInfo.screenWidth/6);
-            int y0 = (AffichageInfo.screenHeight/4);
-            int valDecY = AffichageInfo.screenHeight/9;
-            int valDecX = AffichageInfo.screenWidth/4;
-
-            AffichageInfo.areaTabPseudo = new JTextArea[AffichageInfo.nbJoueur];
-
-            for (int i = 0, decx = 0, decy = 0; i < AffichageInfo.nbJoueur; i++, decy+= valDecY) {
-                AffichageInfo.areaTabPseudo[i] = new JTextArea();
-                if(i<=5){
-                    AffichageInfo.areaTabPseudo[i].setBounds(x0, y0 + decy, AffichageInfo.largeurReponse, AffichageInfo.hauteurReponse);
-                }
-                else{
-                    if(i==6){
-                        decx += AffichageInfo.largeurReponse + valDecX;
-                        decy = 0;
-                    }
-                    AffichageInfo.areaTabPseudo[i].setBounds(x0 + decx, y0 + decy, AffichageInfo.largeurReponse, AffichageInfo.hauteurReponse);
-                }
-                add(AffichageInfo.areaTabPseudo[i]);
-            }
-
             add(AffichageInfo.boutonValiderPseudo);
-            Affichage.MenuConfigPartie(g);
 
+            // Assurez-vous de n'initialiser les composants qu'une seule fois
+            if (AffichageInfo.areaTabPseudo == null) {
+                //initialiserComposants();            }
+
+            Affichage.MenuConfigPartie(g);
         } else if (AffichageInfo.menu == 3) { //menu qui affiche le plateau de jeu
-            remove(AffichageInfo.boutonValiderPseudo);
-            for(int i=0; i<AffichageInfo.nbJoueur; i++) {
+            for (int i = 0; i < AffichageInfo.nbJoueur; i++) {
                 remove(AffichageInfo.areaTabPseudo[i]);
             }
-
-
+            remove(AffichageInfo.boutonValiderPseudo);
         }
-
-    }
+    }*/
 
     /*Fonction pour verifier si tous les pseudos sont differents*/
     private boolean verifierPseudosUniques() {
@@ -123,10 +209,11 @@ public class AffichageConfiguration extends JPanel {
                 return false; // Pseudo déjà rencontré, n'est pas unique
             }
         }
-
         return true; // Tous les pseudos sont uniques
     }
+
     private boolean verifierPseudosNonNuls() {
+        System.out.println("NOmbre de joueur : "+ AffichageInfo.nbJoueur);
         for (JTextArea pseudoTextArea : AffichageInfo.areaTabPseudo) {
             System.out.println("Dans fonction pour verifier pseudo : " + pseudoTextArea.getText().trim());
             if (pseudoTextArea.getText().trim().isEmpty()) {
@@ -136,20 +223,11 @@ public class AffichageConfiguration extends JPanel {
         return true; // Tous les JTextArea sont non nuls
     }
 
-    private String[] recupererValeursJTextArea() {
-        String[] valeurs = new String[AffichageInfo.areaTabPseudo.length];
-
-        for (int i = 0; i < AffichageInfo.areaTabPseudo.length; i++) {
-            valeurs[i] = AffichageInfo.areaTabPseudo[i].getText();
-        }
-
-        return valeurs;
-    }
-
     private void afficherContenuPseudos() {
         System.out.println("Contenu des pseudos : ");
-        for (int i = 0; i < AffichageInfo.areaTabPseudo.length; i++) {
-            System.out.println("Joueur " + (i + 1) + " : " + AffichageInfo.areaTabPseudo[i].getText().trim());
+        for (int i = 0; i < AffichageInfo.nbJoueur; i++) {
+            String texte = AffichageInfo.areaTabPseudo[i].getText();
+            System.out.println("Joueur " + (i + 1) + ": " + texte);
         }
     }
 }
--- CAPI_Projet_implementation diagramme des classes/src/AffichageInfo.java
+++ CAPI_Projet_implementation diagramme des classes/src/AffichageInfo.java
@@ -17,14 +17,16 @@ public class AffichageInfo {
     public static int largeurReponse = screenWidth / 5;
     public static int hauteurReponse = screenHeight / 18;
 
+    /* --------------------Différentes variables--------------------- */
+    public static int nbJoueur = 2;
+
+    /* --------------------Variable menu qui varie--------------------- */
+    public static int menu = 0;
     /* --------------------Differents boutons--------------------- */
     public static JButton boutonNouvellePartie = new JButton("Nouvelle Partie");
-
     public static JButton boutonValiderNbJoueur = new JButton("Valider");
 
     public static JButton boutonValiderPseudo = new JButton("Valider");
-
-    public static JTextArea[] areaTabPseudo ;
 
     /* --------------------Spinner--------------------- */
     public static SpinnerModel spinnerModel = new SpinnerNumberModel(2, 2, 12, 1);
@@ -32,10 +34,11 @@ public class AffichageInfo {
     // Créer un JSpinner avec le SpinnerModel
     public static JSpinner nb_joueur_Spinner = new JSpinner(spinnerModel);
 
-    /* --------------------Différentes variables--------------------- */
-    public static int nbJoueur = 2;
+    /* --------------------TextArea--------------------- */
+    public static JTextArea[] areaTabPseudo = null;
+    public static JLabel labelTitreAccueil = new JLabel("Planning Poker");
+    public static JLabel labelTitreNbJoueur = new JLabel("Configuration de la partie");
+    public static JLabel labelNbJoueur = new JLabel("Nombre de joueur :\n(entre 2 et 12)");
+    public static JLabel labelTitrePseudoJoueur = new JLabel("Choix des pseudos");
 
-    /* --------------------Variable menu qui varie--------------------- */
-    public static int menu = 0;
-    //public static JTextArea nb_joueur =  new JTextArea();
 }
